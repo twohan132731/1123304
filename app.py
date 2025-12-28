38e6efe52dbc4a4ec99f453b27379f8866cc78d6
twohan132731 <ruby2005940331@gmail.com>
2025-12-28 21:49:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Azure Web App!"
+    now = datetime.now(timezone.utc).isoformat()
+    return f"<h1>HW3 - Azure PaaS</h1><p>UTC: {now}</p>"
 
 if __name__ == "__main__":
     app.run()
